cfb5f10379965b8a6312bde47df310ee196818d1
Matt Werth <matthewwerth@gmail.com>
2020-08-01 07:15:39 -0400

    Refactored & Finished Code

--- colorGame.js
+++ colorGame.js
@@ -1,48 +1,55 @@
 let numSquares = 6
-let colors = generateRandomColors(numSquares) //
+let colors = []
+let pickedColor;
 
 let message = document.getElementById('message')
 let squares = document.querySelectorAll('.square')
-let pickedColor = randomColor()
 let colorDisplay = document.getElementById('colorDisplay')
 let h1 = document.querySelector('h1')
 let resetGame = document.getElementById('newColorsButton')
+let modeButton = document.querySelectorAll(".mode")
 
-let easyBtn = document.querySelector("#easyBtn")
-let hardBtn = document.querySelector("#hardBtn")
+init()
 
-easyBtn.addEventListener("click", ()=> {
-    
-    easyBtn.classList.add("selected")
-    hardBtn.classList.remove("selected")
-    numSquares = 3
-    colors = generateRandomColors(numSquares)
-    pickedColor = randomColor()
-    colorDisplay.textContent = pickedColor
-    h1.style.backgroundColor = 'steelblue'
+function init() {
+    setUpModeButtons()
+    setUpSquares()
+    reset()
+}
 
-    for(let i = 0; i < squares.length; i++) {
-        if(colors[i]) squares[i].style.background = colors[i]
-        else squares[i].style.display = 'none'
-    }
-})
+function setUpSquares() {
+  for (let i = 0; i < squares.length; i++) {
+    let ele = squares[i]
+    ele.style.backgroundColor = colors[i]
 
-hardBtn.addEventListener("click", () => {
-    hardBtn.classList.add("selected")
-    easyBtn.classList.remove("selected")
-    numSquares = 6
-    colors = generateRandomColors(numSquares)
-    pickedColor = randomColor()
-    colorDisplay.textContent = pickedColor
-    h1.style.backgroundColor = 'steelblue'
+    ele.addEventListener('click', () => {
+        let clickedColor = ele.style.backgroundColor
+        
+        if (clickedColor === pickedColor) {
+            message.innerText = "YES!"
+            resetGame.innerText = "Play Again?"
+            changeColors(clickedColor)
+            h1.style.backgroundColor = pickedColor
+        } 
+        else ele.style.backgroundColor = 'rgb(10, 27, 41)'
+    })
+  }
+}
 
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.background = colors[i]
-        squares[i].style.display = 'block'
-    }
-})
+function setUpModeButtons() {
 
-resetGame.addEventListener("click", ()=> {
+  for (let i = 0; i < modeButton.length; i++) {
+    modeButton[i].addEventListener("click", () => {
+        modeButton[0].classList.remove("selected")
+        modeButton[1].classList.remove("selected")
+        modeButton[i].classList.add("selected")
+        modeButton[i].textContent === "Easy" ? numSquares = 3 : numSquares = 6
+        reset()
+    })
+  }
+}
+
+function reset() {
     colors = generateRandomColors(numSquares) //
     pickedColor = randomColor()
     colorDisplay.textContent = pickedColor
@@ -52,33 +59,19 @@ resetGame.addEventListener("click", ()=> {
 
     for (let i = 0; i < squares.length; i++) { //REFACTOR DRY
         let ele = squares[i]
+        
+        if(colors[i]) { 
+            ele.style.display = "block"
+            ele.style.backgroundColor = colors[i]
+        } else ele.style.display = "none"
 
         ele.style.backgroundColor = colors[i]
     }
-})
-
-colorDisplay.textContent = pickedColor
-
-for (let i = 0; i < squares.length; i++) {
-    let ele = squares[i]
-
-    ele.style.backgroundColor = colors[i]
-    
-    ele.addEventListener('click', ()=> {
-        
-       let clickedColor = ele.style.backgroundColor
-       
-       if(clickedColor === pickedColor) {
-           message.innerText = "YES!"
-           resetGame.innerText = "Play Again?"
-           changeColors(clickedColor)
-           h1.style.backgroundColor = pickedColor
-
-       } else { 
-           ele.style.backgroundColor = 'rgb(10, 27, 41)'
-       }
-    })
 }
+
+resetGame.addEventListener("click", ()=> {
+    reset()
+})
 
 function changeColors(color) {
     for(let i = 0; i < squares.length; i++) {
